927c8574608d84942ddbe70ac383ac2ffa48638a
Datt2805 <bhattdatt88@gmail.com>
2026-01-28 13:50:49 +0530

    set_files

---
+++ frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }} >
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>Welcome to the Homepage</h1>
+      <p style={{ fontSize: '1.2rem' }}>This is the main landing page of the application.</p>
+    </div>
+  )
+}
+
+export default page
